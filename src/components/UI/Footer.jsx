d99321f7fd5f91e042d39cb5c7acecdc3b37d59a
Stanley Nzegwu <nzegwustanley@gmail.com>
2023-10-13 10:33:29 +0100

    new commit

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -4,6 +4,7 @@
 // import { store } from "../../store";
 // import { useEffect, useRef } from "react";
 // import gsap from "gsap";
+// import upArrow from "/images/up-arrow.png";
 
 // const Footer = () => {
 //   const snap = useSnapshot(store);
@@ -47,8 +48,8 @@
 //   return (
 //     <div
 //       ref={lastSectionRef}
-//       className="bg-gray-300 h-[100vh] min-h-screen w-[100vw] z-50 flex flex-col p-5 text-black bg-yellow-300"
-//       style={{ position: "absolute", top: `650vh`, pointerEvents: "all", touchAction: "auto" }}
+//       className="bg-gray-300 h-[100vh] w-[100vw] max-w-full  min-h-screen  z-50 flex flex-col p-5 text-black"
+//       style={{ position: "fixed", top: `650vh`, right: "0" }}
 //     >
 //       <div className="w-[100%] flex flex-col md:flex-row gap-4 h-[60%]">
 //         <img
@@ -62,20 +63,22 @@
 //           className="w-[100%] md:w-[50%] h-1/2 md:h-full aspect-auto bg-gray-200"
 //         />
 //       </div>
-//       <div className="absolute bottom-[12%] w-full flex flex-col justify-center items-center py-5 border-y-[0.5px] border-[#ffffff] cursor-pointer bg-red-500">
-//         <span>&and;</span>
-//         <span className="text-sm font-semibold" onClick={handleScrollToTop}>
-//           Back to top
-//         </span>
-//       </div>
-//       <div className="absolute bottom-0 w-[100%] flex items-center p-4">
-//         <ul className="font-semibold uppercase w-full flex justify-center gap-2 flex-wrap md:gap-10 text-center">
-//           <li className="">Luxury brand</li>
-//           <li>pens</li>
-//           <li>collection</li>
-//           <li>Legal notice</li>
-//           <li>services</li>
-//         </ul>
+//       <div className="h-[40%]">
+//         <div className="h-1/2 left-0 bottom-[12%] w-full flex flex-col justify-end items-center py-5 border-y-[0.5px] border-[#ffffff]">
+//           <img className="h-[20px] w-[20px]" src={upArrow} alt="upArrow" />
+//           <span className="text-sm font-semibold cursor-pointer" onClick={handleScrollToTop}>
+//             Back to top
+//           </span>
+//         </div>
+//         <div className="h-1/2 bottom-0 left-0 w-[100%] flex items-end p-4">
+//           <ul className="font-semibold uppercase w-full flex justify-center gap-2 flex-wrap md:gap-10 text-center">
+//             <li className="">Luxury brand</li>
+//             <li>pens</li>
+//             <li>collection</li>
+//             <li>Legal notice</li>
+//             <li>services</li>
+//           </ul>
+//         </div>
 //       </div>
 //     </div>
 //   );
@@ -133,38 +136,10 @@ const Footer = () => {
   return (
     <div
       ref={lastSectionRef}
-      className="bg-gray-300 h-[100vh] w-[100vw] max-w-full  min-h-screen  z-50 flex flex-col p-5 text-black"
-      style={{ position: "fixed", top: `700vh`, right: "0" }}
+      className="bg-gray-300 h-[100vh] w-[100%] max-w-full  min-h-screen  z-50  text-black"
+      style={{ position: "absolute", top: `650vh`, right: "0" }}
     >
-      <div className="w-[100%] flex flex-col md:flex-row gap-4 h-[60%]">
-        <img
-          src={penImg1}
-          alt="penBrand"
-          className="w-[100%] md:w-[50%] h-1/2 md:h-full aspect-auto bg-gray-200"
-        />
-        <img
-          src={penImg2}
-          alt="penBrand1"
-          className="w-[100%] md:w-[50%] h-1/2 md:h-full aspect-auto bg-gray-200"
-        />
-      </div>
-      <div className="h-[40%]">
-        <div className="h-1/2 left-0 bottom-[12%] w-full flex flex-col justify-end items-center py-5 border-y-[0.5px] border-[#ffffff]">
-          <img className="h-[20px] w-[20px]" src={upArrow} alt="upArrow" />
-          <span className="text-sm font-semibold cursor-pointer" onClick={handleScrollToTop}>
-            Back to top
-          </span>
-        </div>
-        <div className="h-1/2 bottom-0 left-0 w-[100%] flex items-end p-4">
-          <ul className="font-semibold uppercase w-full flex justify-center gap-2 flex-wrap md:gap-10 text-center">
-            <li className="">Luxury brand</li>
-            <li>pens</li>
-            <li>collection</li>
-            <li>Legal notice</li>
-            <li>services</li>
-          </ul>
-        </div>
-      </div>
+      content
     </div>
   );
 };
